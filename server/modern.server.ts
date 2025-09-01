9ff3239acd5e9c8ae71652c10eb7d784b74526c0
RossLO <wewe381h@gmail.com>
2025-09-01 16:58:07 +0800

    Update modern.server.ts

--- server/modern.server.ts
+++ server/modern.server.ts
@@ -1,6 +1,19 @@
-import { defineServerConfig } from '@modern-js/server-runtime';
+import { defineServerConfig, type MiddlewareHandler } from '@modern-js/server-runtime';
 
-import { modifyResBody } from '../src/middlewares';
+
+// Modify the Response Body
+export const modifyResBody: MiddlewareHandler = async (c, next) => {
+  await next();
+
+  const { res } = c;
+  const text = await res.text();
+  const newText = text.replace('<body>', '<body> <h3>bytedance</h3>');
+
+  c.res = c.body(newText, {
+    status: res.status,
+    headers: res.headers,
+  });
+};
 
 export default defineServerConfig({
   renderMiddlewares: [
